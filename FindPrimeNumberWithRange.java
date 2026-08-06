e75e62eeae205f48c0698bce33949d9a962e073e
Shravan Patel <patelshravan0808@gmail.com>
2026-08-06 09:20:59 +0530

    Change range for prime number calculation to 100
    Updated the range to print all prime numbers from 1 to 100.

--- FindPrimeNumberWithRange.java
+++ FindPrimeNumberWithRange.java
@@ -1,18 +1,20 @@
-class FindPrimeNumberWithRange{
-public static void main(String[] args){
-	int n=10;
-	for(int i=2;i<=n;i++){
+class FindPrimeNumberWithRange {
+    public static void main(String[] args) {
+        int n = 100; // Print all prime numbers from 1 to 100
+
+        for (int i = 2; i <= n; i++) {
             boolean isPrime = true;
-               
-	    for(int j=2; j*j<=i ; j++){
-			if(i%j == 0){
-			      isPrime = false;
-			      break;
-			}			
-	     }
-             if(isPrime){
-                      System.out.println(i);
-             }
-	}
-}
+
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+    }
 }
