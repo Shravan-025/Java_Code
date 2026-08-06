ede5c88b6966412e378d13b033358561b72d3012
Shravan Patel <patelshravan0808@gmail.com>
2026-08-06 09:21:18 +0530

    Refactor DESIGN.java to print diamond pattern
    Updated the program to print a diamond shape based on odd number of rows input.

--- DESIGN.java
+++ DESIGN.java
@@ -1,29 +1,36 @@
 import java.util.Scanner;
-class DESIGN{
-     public static void main(String[] args){
-        Scanner scr = new Scanner(System.in);
-        System.out.print("Enter Number of line");
-        int n = scr.nextInt();
-        for(int i=1; i<=n; i++){
-            for(int j=1;j<n-i; j++){
+
+class DESIGN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter odd number of rows: ");
+        int n = sc.nextInt();
+
+        int mid = (n + 1) / 2;
+
+        // Upper half
+        for (int i = 1; i <= mid; i++) {
+            for (int j = 1; j <= (mid - i); j++) {
                 System.out.print(" ");
             }
-            for(int j=1;j<=((i*2)-1);j++){
+            for (int j = 1; j <= (2 * i - 1); j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        for(int i=n-1; i>=1;i--){
-            for(int j=1; j<=n-i;j++){
+        // Lower half
+        for (int i = mid - 1; i >= 1; i--) {
+            for (int j = 1; j <= (mid - i); j++) {
                 System.out.print(" ");
             }
-            for(int j=1;j<=((i*2)-1);j++){
+            for (int j = 1; j <= (2 * i - 1); j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        
-        scr.close();
-     }
+
+        sc.close();
+    }
 }
